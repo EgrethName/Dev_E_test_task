2f16a6d3c39e75e2ccd07daa8bbeb36b3132ca03
Anna Skorykh <egreth@mail.ru>
2021-05-12 00:58:14 +0300

    update urls

--- Dev_E_test_task/urls.py
+++ Dev_E_test_task/urls.py
@@ -1,12 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from Dev_E_test_task_app.views import UserViewSet, UserModelViewSet
+from Dev_E_test_task_app.views import UserModelViewSet
 
 
 router = DefaultRouter()
-router.register('api/v1/users', UserViewSet, basename='api/v1/users/')
-router.register('api/v1/users1', UserModelViewSet, basename='api/v1/users1/')
+router.register('users', UserModelViewSet)
 
 
 urlpatterns = [
--- Dev_E_test_task_app/urls.py
+++ Dev_E_test_task_app/urls.py
@@ -1,28 +1,17 @@
-from django.urls import path, re_path
-from rest_framework.authtoken import views
+from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 
-from .views import UsersRegistrationAPIView, UserViewSet, UserModelViewSet, TokenAuthenticationView
+from .views import UserListCreateViewSet, UserModelViewSet, TokenAuthenticationView
 
 
 app_name = 'Dev_E_test_task_app'
 
-user_list = UserViewSet.as_view({
-    'get': 'list',
-    'post': 'create'
-})
-user_detail = UserViewSet.as_view({
-    'get': 'retrieve',
-    'put': 'update',
-    'patch': 'partial_update',
-    'delete': 'destroy'
-})
 
-user_list1 = UserModelViewSet.as_view({
+user_list = UserListCreateViewSet.as_view({
     'get': 'list',
     'post': 'create'
 })
-user_detail1 = UserModelViewSet.as_view({
+user_detail = UserModelViewSet.as_view({
     'get': 'retrieve',
     'put': 'update',
     'patch': 'partial_update',
@@ -30,10 +19,7 @@ user_detail1 = UserModelViewSet.as_view({
 })
 
 urlpatterns = format_suffix_patterns([
-    # path('api/v1/users/', UsersRegistrationAPIView.as_view()),
-    path('api-token-auth/', TokenAuthenticationView.as_view()),
-    path('users/', user_list, name='user-list'),
-    path('users/<int:pk>/', user_detail, name='user-detail'),
-    path('users1/', user_list1, name='user-list'),
-    path('users1/<int:pk>/', user_detail1, name='user-detail'),
+    path('api-token-auth/', TokenAuthenticationView.as_view(), name='api-token-auth'),
+    path('api/v1/users/', user_list, name='user-list'),
+    path('api/v1/users/<int:pk>/', user_detail, name='user-detail'),
 ])
